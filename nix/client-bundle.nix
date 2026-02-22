b5585ee1f074d8b94273aae63ab7af57532f8119
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-02-22 12:08:04 +0000

    fix: handle git dependencies in nix client bundle
    Filter spago.lock packages by type: registry packages use fetchurl
    with integrity hashes, git packages (keri-purs) use fetchgit with
    explicit hashes. Fixes docker image build failure.

--- nix/client-bundle.nix
+++ nix/client-bundle.nix
@@ -1,9 +1,14 @@
 { pkgs }:
 let
-  lockFile = builtins.fromJSON (builtins.readFile ../client/spago.lock);
+  lockFile = builtins.fromJSON
+    (builtins.readFile ../client/spago.lock);
 
-  # Fetch each PureScript package from the registry using spago.lock
-  # integrity hashes (SRI sha256, verified against tarballs)
+  # Filter to registry packages (have version + integrity)
+  registryPackages = pkgs.lib.filterAttrs
+    (_: info: info.type or "" == "registry")
+    lockFile.packages;
+
+  # Fetch each registry package using spago.lock integrity
   pursPackages = builtins.mapAttrs
     (name: info:
       pkgs.fetchurl {
@@ -11,35 +16,55 @@ let
           "https://packages.registry.purescript.org/${name}/${info.version}.tar.gz";
         hash = info.integrity;
       })
-    lockFile.packages;
+    registryPackages;
+
+  # Git dependencies (like keri-purs)
+  keriPurs = pkgs.fetchgit {
+    url = "https://github.com/paolino/keri-purs.git";
+    rev = "25a4a9887d6414e0634c6c4ed61aead8d1f6d852";
+    hash =
+      "sha256-fAhr8SPHOzfugixGRukdHx1Ah7cqHODZNWZy0KiSq0o=";
+  };
 
   # Assemble all PureScript packages into a single directory
   pursPackagesDir = pkgs.runCommand "purs-packages" { } (
     "mkdir -p $out\n"
-    + builtins.concatStringsSep "\n" (pkgs.lib.mapAttrsToList
-      (name: info:
-        let tarball = pursPackages.${name};
-        in ''
-          mkdir -p $out/${name}-${info.version}
-          tar xzf ${tarball} -C $out/${name}-${info.version} \
-            --strip-components=1
-        '')
-      lockFile.packages));
+    + builtins.concatStringsSep "\n"
+      (pkgs.lib.mapAttrsToList
+        (name: info:
+          let tarball = pursPackages.${name};
+          in ''
+            mkdir -p $out/${name}-${info.version}
+            tar xzf ${tarball} \
+              -C $out/${name}-${info.version} \
+              --strip-components=1
+          '')
+        registryPackages)
+    + "\n"
+    + ''
+      mkdir -p $out/keri-purs
+      cp -r ${keriPurs}/src $out/keri-purs/src
+    '');
 
   # npm dependencies (FFI runtime deps bundled by esbuild)
   tweetnacl = pkgs.fetchurl {
-    url = "https://registry.npmjs.org/tweetnacl/-/tweetnacl-1.0.3.tgz";
-    hash = "sha256-X43EnLRIPiBro+uyKrvM/AIYxZtg/HjM4kGXztW54QI=";
+    url =
+      "https://registry.npmjs.org/tweetnacl/-/tweetnacl-1.0.3.tgz";
+    hash =
+      "sha256-X43EnLRIPiBro+uyKrvM/AIYxZtg/HjM4kGXztW54QI=";
   };
   blakejs = pkgs.fetchurl {
-    url = "https://registry.npmjs.org/blakejs/-/blakejs-1.2.1.tgz";
-    hash = "sha256-P1AQJnwaPFI0/G6xGCDqitY6HGX0R8cTXCz687ugHt4=";
+    url =
+      "https://registry.npmjs.org/blakejs/-/blakejs-1.2.1.tgz";
+    hash =
+      "sha256-P1AQJnwaPFI0/G6xGCDqitY6HGX0R8cTXCz687ugHt4=";
   };
 in pkgs.stdenvNoCC.mkDerivation {
   name = "kel-circle-client-bundle";
   src = ../client;
 
-  nativeBuildInputs = [ pkgs.purs pkgs.esbuild pkgs.nodejs_20 ];
+  nativeBuildInputs =
+    [ pkgs.purs pkgs.esbuild pkgs.nodejs_20 ];
 
   buildPhase = ''
     shopt -s globstar
@@ -52,7 +77,7 @@ in pkgs.stdenvNoCC.mkDerivation {
     tar xzf ${blakejs} -C node_modules/blakejs \
       --strip-components=1
 
-    # Compile PureScript (sources + all registry deps)
+    # Compile PureScript (sources + all deps)
     purs compile \
       kel-circle-client/src/**/*.purs \
       kel-circle-trivial/src/**/*.purs \
